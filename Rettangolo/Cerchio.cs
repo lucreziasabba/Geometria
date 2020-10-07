511e111bfbf6af01c15bdef5101adf4fc4c5df31
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2020-10-07 12:05:43 +0200

    Add project files.

---
+++ Rettangolo/Cerchio.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Geometria
+{
+    public class Cerchio
+    {
+        public static double PI = 3.14;
+
+        public double raggio;
+
+        public Cerchio(double r)
+        {
+            raggio = r;
+        }
+
+        public double CalcolaCirconferenza()
+        {
+            return 2 * PI * raggio;
+        }
+
+        public double CalcolaArea()
+        {
+            return PI * raggio * raggio;
+        }
+    }
+}
---
+++ Rettangolo/Rettangolo.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Geometria
+{
+    public class Rettangolo
+    {
+        private int x;
+        private int y;
+        private int width;
+        private int height;
+
+        public Rettangolo(int x, int y, int w, int h)
+        {
+            this.x = x;
+            this.y = y;
+            this.width = w;
+            this.height = h;
+        }
+
+        public int CalcolaArea()
+        {
+            return width * height;
+        }
+
+        public int CalcolaPerimetro()
+        {
+            return height * 2 + width * 2;
+        }
+
+        public bool Contiene(Rettangolo r)
+        {
+            if(x<=r.x && y <= r.y && x + width >= r.x + r.width && y+height >= r.y + r.height)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+    }
+    
+
+}
